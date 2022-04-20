634460a98fc85eb393195b0835fe4976f61789f4
Kyaw-Zayar <81757007+Kyaw-Zayar@users.noreply.github.com>
2022-04-21 01:33:46 +0630

    Final Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const [emojis, setEmojis] = useState([]);
+    const [keyword, setKeyword] = useState('')
+
+    useEffect(() => {
+        fetch('https://emoji-api.com/emojis?access_key=eabc2dc3c3d0e7ee1495e9825f7f9c705db2077b')
+            .then(res => res.json())
+            .then(data => setEmojis(data))
+    }, [])
+
+    const handleSearch = (e) => {
+        setKeyword(e.target.value)
+    }
+
+    let handleSubmit = () => {
+        if (keyword !== '') {
+            fetch(`https://emoji-api.com/emojis?search=${keyword}&access_key=eabc2dc3c3d0e7ee1495e9825f7f9c705db2077b`)
+                .then(res => res.json())
+                .then(data => setEmojis(data))
+        }
+    }
+
+
+    return (
+        <div className='app'>
+            <div className="menu">
+                <div className="menu_text">
+                    <h1>Emoji Search</h1>
+                    <p>Emoji Search with React.js</p>
+                    <div>
+                        <input type="text" placeholder='Search Emoji' value={keyword} onChange={(e) => handleSearch(e)}/>
+                        <button className='search' onClick={handleSubmit}>Search</button>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                {emojis.map(emoji =>
+                    <div className="card" key={emoji.slug}>
+                        <p className="emoji">
+                            {emoji.character}
+                        </p>
+                        <p className="name">
+                            {emoji.unicodeName}
+                        </p>
+                    </div>
+                )}
+
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
